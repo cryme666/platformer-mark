3d59f0a3cecab534aa9034f1e153481ec2f9f94f
cryme666 <125838508+cryme666@users.noreply.github.com>
2025-03-06 19:04:14 +0200

    upd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
-from os.path import join
+from os.path import join,isfile
+from os import listdir
+
 pygame.init()
 
 WIDTH = 800
@@ -9,80 +11,209 @@ pygame.display.set_caption("Platformer")
 
 FPS = 60
 
-class Object(pygame.sprite.Sprite):
-    def __init__(self,x,y,width,height,name = None):
+
+def flip(sprites):
+    return [pygame.transform.flip(sprite,True,False) for sprite in sprites]
+
+def load_sprite_sheets(dir1,dir2,width,height,direction = None):
+    path = join("platformer-assets-main",dir1 ,dir2)
+    images = [f for f in listdir(path) if isfile(join(path,f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path,image)).convert_alpha()
+
+        sprites = []
+
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width,0,width,height)
+            surface.blit(sprite_sheet,(0,0),rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+            if direction:
+                all_sprites[image.replace('.png','') + '_right'] = sprites
+                all_sprites[image.replace('.png','') + '_left'] = flip(sprites)
+            else:
+                all_sprites[image.replace('.png','')] = sprites
+    return all_sprites 
+
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+    ANIMATION_DELAY = 3
+
+    def __init__(self, x, y, width, height):
         super().__init__()
-        self.rect = pygame.Rect(x,y,width,height)
-        self.image = pygame.Surface((width,height),pygame.SRCALPHA)
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+        self.jump_count = 0
+        self.hit = False
+        self.hit_count = 0
+        self.sprite = self.SPRITES["idle_right"][0]
+
+    def jump(self):
+        self.y_vel = -self.GRAVITY * 8
+        self.animation_count = 0
+        self.jump_count += 1
+        if self.jump_count == 1:
+            self.fall_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def make_hit(self):
+        self.hit = True
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+
+        if self.hit:
+            self.hit_count += 1
+        if self.hit_count > fps * 2:
+            self.hit = False
+            self.hit_count = 0
+
+        self.fall_count += 1
+        self.update_sprite()
+
+    def landed(self):
+        self.fall_count = 0
+        self.y_vel = 0
+        self.jump_count = 0
+
+    def hit_head(self):
+        self.count = 0
+        self.y_vel *= -1
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.hit:
+            sprite_sheet = "hit"
+        elif self.y_vel < 0:
+            if self.jump_count == 1:
+                sprite_sheet = "jump"
+            elif self.jump_count == 2:
+                sprite_sheet = "double_jump"
+        elif self.y_vel > self.GRAVITY * 2:
+            sprite_sheet = "fall"
+        elif self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count //
+                        self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+        self.update()
+
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
+
+    def draw(self, win):
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
+
+    
+
+
+
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
         self.width = width
         self.height = height
         self.name = name
 
-    def draw(self,window):
-        window.blit(self.image,(self.rect.x,self.rect.y))
+    def draw(self, window):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Block(Object):
-    def __init__(self,x,y,size):
-        super().__init__(x,y,size,size,name = "Block")
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size, name="Block")
         block = get_block(size)
-        self.image.blit(block,(0,0))
+        self.image.blit(block, (0, 0))
         self.mask = pygame.mask.from_surface(self.image)
 
-
-
 def get_background(name):
-    image = pygame.image.load(join("platformer-assets-main","Background",name))
-    _,_,width,height = image.get_rect()
+    image = pygame.image.load(join("platformer-assets-main", "Background", name))
+    _, _, width, height = image.get_rect()
 
     background = []
-    
-    for i in range(WIDTH//width + 1):
-        for j in range(HEIGHT//height+1):
-            pos = (i*width,j*height)
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
             background.append(pos)
 
-    return background,image
+    return background, image
 
 def get_block(size):
-    path = join('platformer-assets-main','Terrain','Terrain.png')
+    path = join('platformer-assets-main', 'Terrain', 'Terrain.png')
 
     image = pygame.image.load(path).convert_alpha()
-    surface = pygame.Surface((size,size),pygame.SRCALPHA,32)
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
 
-    rect = pygame.Rect(96,0,size,size)
-    surface.blit(image,(0,0),rect)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
 
     return pygame.transform.scale2x(surface)
 
-
-
-
-def draw(window, background,bg_image,objects):
+def draw(window, background, bg_image, player,objects):
     for positions in background:
-        window.blit(bg_image,positions)
+        window.blit(bg_image, positions)
 
     for object in objects:
         object.draw(window)
 
+    player.draw(window)
 
 def main(window):
-    pygame.time.Clock().tick(FPS)
+    clock = pygame.time.Clock()
 
-    background,image = get_background('Blue.png')
+    background, image = get_background('Blue.png')
+
+    player = Player(100,100,50,50)
 
     block_size = 96
 
-    block = Block(0,0,block_size)
+    blocks = []
+    for i in range(WIDTH // block_size + 1):
+        blocks.append(Block(i * block_size, HEIGHT - block_size, block_size))
 
     running = True
 
     while running:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # Оновлення екрану
-        draw(screen,background,image,[block])
+        draw(screen, background, image, player,blocks)
         pygame.display.update()
 
     pygame.quit()
