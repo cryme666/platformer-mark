07069c9e7337fa95db24fe08a4c6e4164002becb
cryme666 <125838508+cryme666@users.noreply.github.com>
2025-03-11 19:03:25 +0200

    upd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from os.path import join,isfile
+from os.path import join, isfile
 from os import listdir
 
 pygame.init()
@@ -10,40 +10,45 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Platformer")
 
 FPS = 60
+PLAYER_VEL = 5
 
 
 def flip(sprites):
-    return [pygame.transform.flip(sprite,True,False) for sprite in sprites]
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
-def load_sprite_sheets(dir1,dir2,width,height,direction = None):
-    path = join("platformer-assets-main",dir1 ,dir2)
-    images = [f for f in listdir(path) if isfile(join(path,f))]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=None):
+    path = join("platformer-assets-main", dir1, dir2)
+
+
+    images = [f for f in listdir(path) if isfile(join(path, f))]
 
     all_sprites = {}
 
     for image in images:
-        sprite_sheet = pygame.image.load(join(path,image)).convert_alpha()
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
 
         sprites = []
 
         for i in range(sprite_sheet.get_width() // width):
             surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
-            rect = pygame.Rect(i * width,0,width,height)
-            surface.blit(sprite_sheet,(0,0),rect)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
             sprites.append(pygame.transform.scale2x(surface))
 
             if direction:
-                all_sprites[image.replace('.png','') + '_right'] = sprites
-                all_sprites[image.replace('.png','') + '_left'] = flip(sprites)
+                all_sprites[image.replace('.png', '') + '_right'] = sprites
+                all_sprites[image.replace('.png', '') + '_left'] = flip(sprites)
             else:
-                all_sprites[image.replace('.png','')] = sprites
-    return all_sprites 
+                all_sprites[image.replace('.png', '')] = sprites
+    return all_sprites
 
 
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
     ANIMATION_DELAY = 3
+    GRAVITY = 1
 
     def __init__(self, x, y, width, height):
         super().__init__()
@@ -132,12 +137,8 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
         self.mask = pygame.mask.from_surface(self.sprite)
-
     def draw(self, win):
         win.blit(self.sprite, (self.rect.x, self.rect.y))
-
-    
-
 
 
 class Object(pygame.sprite.Sprite):
@@ -152,12 +153,14 @@ class Object(pygame.sprite.Sprite):
     def draw(self, window):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
 class Block(Object):
     def __init__(self, x, y, size):
         super().__init__(x, y, size, size, name="Block")
         block = get_block(size)
         self.image.blit(block, (0, 0))
         self.mask = pygame.mask.from_surface(self.image)
+
 
 def get_background(name):
     image = pygame.image.load(join("platformer-assets-main", "Background", name))
@@ -172,6 +175,7 @@ def get_background(name):
 
     return background, image
 
+
 def get_block(size):
     path = join('platformer-assets-main', 'Terrain', 'Terrain.png')
 
@@ -183,7 +187,8 @@ def get_block(size):
 
     return pygame.transform.scale2x(surface)
 
-def draw(window, background, bg_image, player,objects):
+
+def draw(window, background, bg_image, player, objects):
     for positions in background:
         window.blit(bg_image, positions)
 
@@ -192,28 +197,83 @@ def draw(window, background, bg_image, player,objects):
 
     player.draw(window)
 
+def handle_vertical_collision(player, objects, dy):
+        collided_objects = []
+        for obj in objects:
+            if pygame.sprite.collide_mask(player, obj):
+                if dy > 0:
+                    player.rect.bottom = obj.rect.top
+                    player.landed()
+                elif dy < 0:
+                    player.rect.top = obj.rect.bottom
+                    player.hit_head()
+                collided_objects.append(obj)
+
+        return collided_objects
+
+def handle_move(player,objects):
+    keys = pygame.key.get_pressed()
+    print(keys[pygame.K_LEFT])
+
+    player.x_vel = 0
+
+    # collide_left = coll
+    # todo написати collide_left та collide_right, тобто повинні бути функції які повертають
+    # todo об'єкти з якими взаємодіє гравець зліва та справа
+
+
+
+
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+        
+    elif keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
+
+    vertical_collide = handle_vertical_collision(player,objects,player.y_vel)
+
+    to_check = [*vertical_collide] #todo тут потрібно також добавити collide_left та collide right
+
+    # for obj in to_check:
+    #     if obj and obj.name == 
+
+
+
+
 def main(window):
     clock = pygame.time.Clock()
 
     background, image = get_background('Blue.png')
 
-    player = Player(100,100,50,50)
-
     block_size = 96
 
-    blocks = []
+    player = Player(100, HEIGHT - block_size - 64, 50, 50)
+
+    floor = []
     for i in range(WIDTH // block_size + 1):
-        blocks.append(Block(i * block_size, HEIGHT - block_size, block_size))
+        floor.append(Block(i * block_size, HEIGHT - block_size, block_size))
+
+    block = Block(block_size*2,HEIGHT - block_size * 2, block_size)
+    block1 = Block(block_size*2,HEIGHT - block_size * 3, block_size)
+
+    objects = [*floor,block,block1]
 
     running = True
 
+    vel = 5
+
+  
     while running:
         clock.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        draw(screen, background, image, player,blocks)
+        player.loop(FPS)
+        handle_move(player,objects)
+        draw(screen, background, image, player, objects)
         pygame.display.update()
 
     pygame.quit()
